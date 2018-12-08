429a6f01eed3b025d73bee0278e96b996a280ec0
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-08 13:53:46 +0100

    string regex tests

--- src/test/java/RegexTest.java
+++ src/test/java/RegexTest.java
@@ -8,8 +8,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -17,14 +16,14 @@ import static org.junit.Assert.assertTrue;
  * Created by mtumilowicz on 2018-12-08.
  */
 public class RegexTest {
-    
+
     @Test
     public void string_matches() {
         // hour
         assertTrue("1:11".matches("[0-2]?\\d:[0-5]\\d"));
         assertTrue("9:11".matches("[0-2]?\\d:[0-5]\\d"));
         assertTrue("0:11".matches("[0-2]?\\d:[0-5]\\d"));
-        
+
         // minute
         assertTrue("1:00".matches("[0-2]?\\d:[0-5]\\d"));
         assertTrue("9:01".matches("[0-2]?\\d:[0-5]\\d"));
@@ -37,33 +36,42 @@ public class RegexTest {
         assertTrue("21:00".matches("[0-2]?\\d:[0-5]\\d"));
         assertFalse("30:00".matches("[0-2]?\\d:[0-5]\\d"));
     }
-    
+
     @Test
     public void string_split() {
-        "a".split("a");
+        String namesContainer = "Michal--|--Marcin--|--Wojtek--|--Ania";
+        String[] names = namesContainer.split("--\\|--");
+
+        assertThat(names, is(new String[]{"Michal", "Marcin", "Wojtek", "Ania"}));
     }
-    
+
     @Test
     public void string_split_limit() {
-        "a".split("a", 1);
+        String[] names = "Michal--|--Marcin--|--Wojtek--|--Ania".split("--\\|--", 3);
+
+        assertThat(names, is(new String[]{"Michal", "Marcin", "Wojtek--|--Ania"}));
     }
-    
+
     @Test
     public void string_replaceAll() {
-        "a".replaceAll("a", "a");
+        String transformed = "Michal--|--Marcin--|--Wojtek--|--Ania".replaceAll("--\\|--", "|");
+
+        assertThat(transformed, is("Michal|Marcin|Wojtek|Ania"));
     }
-    
+
     @Test
     public void string_replaceFirst() {
-        "a".replaceFirst("a", "a");
+        String transformed = "Michal--|--Marcin--|--Wojtek--|--Ania".replaceFirst("--\\|--", "|");
+
+        assertThat(transformed, is("Michal|Marcin--|--Wojtek--|--Ania"));
     }
 
     @Test
     public void pattern_emails() throws IOException {
         Pattern emailPattern = Pattern.compile("[_.\\w]+@([\\w]+\\.)+[\\w]{2,20}", Pattern.CASE_INSENSITIVE);
-        
+
         List<String> emails;
-        
+
         try (var reader = Files.newBufferedReader(Path.of("emails.txt"))) {
             emails = reader.lines()
                     .filter(emailPattern.asMatchPredicate())
